6a916b730b98177e40ea977db99489c9a1b805f9
Hardik Bhanot <76391935+Hardikbhanot@users.noreply.github.com>
2021-08-10 20:49:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
+const svg= document.querySelector(".svg")
+    const width= window.innerWidth
+    console.log(width)
+    if(width<=768){
+        svg.innerHTML=""
+    }
+const burger = document.querySelector(".burger")
+const links =document.querySelector(".nav-links")
+const link =document.querySelectorAll(".nav-link")
+
+burger.addEventListener('click',()=>{
+    links.classList.toggle("open")
+    link.classList.toggle("fade")
+})
+
 window.addEventListener('scroll', function () {
     let header = document.querySelector('header');
     let windowPosition = window.scrollY > 0;
     header.classList.toggle('scrolling-active', windowPosition);
 })
-function navSlide() {
-    let burger=document.querySelector(".burger");
-    let nav=document.querySelector(".nav-list");
-    let navlinks=document.querySelectorAll(".nav-list li");
-
-    burger.addEventListener("click",function(){
-        nav.classList.toggle("nav-active");
-       
-
-        // Animate Links
-        navlinks.forEach((link,index) => {
-            if(link.style.aniamtion){
-                link.style.aniamtion='';}
-           else{
-               link.style.animation = `nav-linkFade 4s ease forwards ${index/7 + 3 }s`;
-           }
-        });
-
-// burger animation
-burger.classList.toggle("toggle");
-
-})
-};
-navSlide();
